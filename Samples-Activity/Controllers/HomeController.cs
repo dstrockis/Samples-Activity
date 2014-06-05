aa8a91b4b89ed879bab224c1800f27cc8819ae77
dstrockis <d.l.strockis@gmail.com>
2014-06-04 18:12:03 -0700

    GitHub API Practice

--- Samples-Activity/Controllers/HomeController.cs
+++ Samples-Activity/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Samples_Activity.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,9 @@ namespace Samples_Activity.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var model = new RepoList("AzureADSamples");
+    
+            return View(model);
         }
 
         public ActionResult About()
---
+++ Samples-Activity/Models/RepoList.cs
@@ -0,0 +1,32 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Samples_Activity.Models
+{
+    public class RepoList
+    {
+        public List<string> repos { get; set; }
+        public RepoList(string GitHubOrg)
+        {
+            repos = new List<string>();
+
+            //var github = new GitHubClient(new ProductHeaderValue("AADSamplesActivity"));
+            //var org = await github.Organization.Get(GitHubOrg);
+            //var pubRepos = org.PublicRepos.
+
+            var repoClient = new RepositoriesClient(new ApiConnection(new Connection(new ProductHeaderValue("AADSamplesActivity"))));
+            var reposPagedCollection = repoClient.GetAllForOrg(GitHubOrg);
+            var repoIEnumerator = reposPagedCollection.Result.GetEnumerator();
+
+            int i=0;
+            while (repoIEnumerator.MoveNext())
+            {
+                repos.Add(repoIEnumerator.Current.Name);
+                i++;
+            }
+        }
+    }
+}
